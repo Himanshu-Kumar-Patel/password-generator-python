283c7c001516db5ec188216f8eb01d1dc85b9004
Himanshu Kumar Patel <himanshupatilxxx@gmail.com>
2026-06-22 14:29:54 +0530

    Add symbol support

--- password_generator.py
+++ password_generator.py
@@ -3,7 +3,7 @@ import string
 
 length = int(input("Enter password length: "))
 
-characters = string.ascii_letters + string.digits
+characters = string.ascii_letters + string.digits + string.punctuation
 
 password = ""
 
